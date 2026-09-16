55dbda76a537fc8d82921dff3f2d6db7e8de6770
ahmedhmusa <ahmedh033@gmail.com>
2026-09-17 00:33:12 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,8 @@ const APP_SHELL = [
   './styles.css',
   './app.js',
   './manifest.json',
-  './icon.svg'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
